70de36c1a161ebd18538d0425885317cff4c1796
kimjayluta <kimjayluta0721@gmail.com>
2020-02-04 12:48:12 +0800

    Added a function that inserts product information to localStorage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Products = [
+  {
+    name: "iPad",
+    price: 200
+  },
+  {
+    name: "iPhone",
+    price: 650
+  }
+];
+
+localStorage.setItem("prod", JSON.stringify(Products));
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Products Manager</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
